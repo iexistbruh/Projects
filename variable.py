a03a308d7c2ab242b3e75bbd1b0ef0c3cf2c44b1
iexistbruh <farwahrumisa@gmail.com>
2025-01-17 11:38:43 +0600

    initial commit

---
+++ variable.py
@@ -0,0 +1,13 @@
+#creating different variable
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+#user input
+name = input("enter your name: ")
+
+print("\nHello", name)
---
+++ welcome.py
@@ -0,0 +1,14 @@
+#print a Sentence
+print("Welcome to the world of programming!")
+
+#print a Number
+print(4)
+
+#Next line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of print Statment
+print("welcome to", end="*")
