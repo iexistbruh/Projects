8d64778ab8295296964f5404b390b0b7f7802f0b
iexistbruh <farwahrumisa@gmail.com>
2025-01-25 21:18:32 +0600

    initial commit

---
+++ string operation.py
@@ -0,0 +1,11 @@
+#input a word
+text = str(input("Enter a string"))
+
+#reverse string
+# using step value as -1 to iterate in reverse
+revTest = text[::-1]
+text = revTest
+
+print("reverse of given string is:")
+print (text)
+
---
+++ typecasting.py
@@ -0,0 +1,24 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("Name:", name)
+print("Data type of Name is", type(name))
+
+print("Age:", age)
+print("Data type of Age is", type(age))
+
+print("is_student:", is_student)
+print("Data type of is_student is", type(is_student))
+
+print("weigth:", weight)
+print("Data type of weigth is", type(weight))
+
+print("\n after type casting")
+age = str(age)
+print (age)
+print("Data type of Age is", type(age))
+weight = int(weight)
+print (weight)
+print("Data type of Weigth is", type(weight))
