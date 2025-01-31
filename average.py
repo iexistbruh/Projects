dbe8a16da59e8b4f512a1c7afd93dfd9ce6af28b
iexistbruh <farwahrumisa@gmail.com>
2025-01-31 11:41:59 +0600

    initial commit

---
+++ average.py
@@ -0,0 +1,14 @@
+#store value
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+
+#finding the total of trees
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("the sum of all the 5 trees are:", sum)
+
+#finding the average of trees
+mean = sum/5
+print("the mean of all the 5 trees are:", mean)
