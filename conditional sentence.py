be1118afe7bb37dc6c9c4d82fb646a9606700ba0
iexistbruh <farwahrumisa@gmail.com>
2025-02-07 11:38:03 +0600

    initial commit

---
+++ conditional sentence.py
@@ -0,0 +1,11 @@
+num1 = 3
+if num1 > 0:
+    print("positive number")
+else:
+    print("negative number")
+
+num2 = -1
+if num2 > 0:
+    print("positive number")
+else:
+    print("negative number")
