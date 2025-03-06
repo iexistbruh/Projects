9a96f9e29adee24f77e31b749a46998f9aae0be9
iexistbruh <155438942+iexistbruh@users.noreply.github.com>
2025-03-06 21:17:12 +0600

    birthday.py

---
+++ birthday.py
@@ -0,0 +1,12 @@
+bday1 = "January 2, 2010"
+bday2 = "March 26, 2010"
+bday3 = "July 25, 2012"
+bday4 = "November 24, 2011"
+bday5 = "December 30, 2011"
+
+print("Stored Birthday Dates:")
+print("1.", bday1)
+print("2.", bday2)
+print("3.", bday3)
+print("4.", bday4)
+print("5.", bday5)
