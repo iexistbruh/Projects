1a3e940b86aba37518a47ac9cf2f5985c9160090
iexistbruh <farwahrumisa@gmail.com>
2025-04-11 12:25:15 +0600

    initial commit

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def add(P, Q):
+    return P+Q
+def subtract(P, Q):
+    return P-Q
+def multiply(P, Q):
+    return P*Q
+def divide(P, Q):
+    return P/Q
+
+print("Please select the operation")
+print("A. Add")
+print("B. Subtract")
+print("C. Multiply")
+print("D. Divide")
+
+choice = input("Pls enter choice(A/ B/ C/ D): ")
+
+num_1 = int(input("ples enter the first number: "))
+num_2 = int(input("ples enter the second number: "))
+
