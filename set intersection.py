1f6ee97eb284c4888a64feae5a417eed6f06992e
iexistbruh <farwahrumisa@gmail.com>
2025-05-31 09:57:54 +0600

    initial commit

---
+++ set intersection.py
@@ -0,0 +1,8 @@
+setx = {"green","blue"}
+sety = {"blue", "yellow"}
+print("original set elements: ")
+print(setx)
+print(sety)
+print("\nIntersection of 2 mentioned set")
+setz = setx.intersection(sety)
+print(setz)
